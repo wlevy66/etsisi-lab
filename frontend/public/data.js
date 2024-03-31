b0df5358b87e2e231eb56b488c88c1234d6e9484
William <williamdlrq.dev@gmail.com>
2024-03-31 20:20:49 +0200

    config some validations on backend

--- frontend/public/data.js
+++ frontend/public/data.js
@@ -1,29 +1,6 @@
-export const USERS = [
-    {
-        username: 'user1',
-        password: 'user1',
-        role: 'professor'
-    },
-    {
-        username: 'user2',
-        password: 'user2',
-        role: 'student'
-    },
-    {
-        username: 'user3',
-        password: 'user3',
-        role: 'professor'
-    },
-    {
-        username: 'user4',
-        password: 'user4',
-        role: 'student'
-    }
-]
-
 export const URL = {
     ROOM: 'http://localhost:3900/api/room/rooms',
-    SCHEDULE: 'http://localhost:3900/api/schedule/schedule',
-    RESERVATION: 'http://localhost:3900/api/reservation/reservation',
+    SCHEDULE: 'http://localhost:3900/api/schedule/schedules',
+    RESERVATION: 'http://localhost:3900/api/reservation/reservations',
     USER: 'http://localhost:3900/api/user'
 }
