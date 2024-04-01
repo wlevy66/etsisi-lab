0a985d82079cdc358e2e776bbada476a237eb270
William Delosrios <williamdlrq.dev@gmail.com>
2024-04-01 23:24:35 +0200

    some commits

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connection = async () => {
     try{
-        await mongoose.connect("mongodb://localhost/lab")
+        await mongoose.connect("mongodb://localhost:27017/etsisi")
 
     } catch (error){
         console.log(error)
