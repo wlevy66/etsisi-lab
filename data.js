91768523574b58dabfd2afe05c188ee7c9fa13ee
William Delosrios <williamdlrq.dev@gmail.com>
2025-03-17 01:06:27 +0100

    docker files updated

--- data.js
+++ data.js
@@ -1,27 +1,13 @@
+db = db.getSiblingDB('etsisi')
 
-db = db.getSiblingDB('etsisi');
-
-db.rooms.insertMany([{
-    "_id": ObjectId ("65fe271ca2caa41d8957c38f"),
-    "name": "Room1",
-    "capacity": 2,
-    "createdAt": {
-      "$date": "2024-03-23T00:49:32.692Z"
-    },
-    "updatedAt": {
-      "$date": "2024-03-23T00:49:32.692Z"
-    },
-    "__v": 0
-  },
-  {
-    "_id": ObjectId ("65fe273ba2caa41d8957c395"),
-    "name": "Room3",
-    "capacity": 3,
-    "createdAt": {
-      "$date": "2024-03-23T00:50:03.213Z"
-    },
-    "updatedAt": {
-      "$date": "2024-03-23T00:50:03.213Z"
-    },
-    "__v": 0
-  }]);
+db.users.insertOne({
+  "name": "Admin",
+  "lastname": "User",
+  "phone": "123456789",
+  "email": "admin@admin.es",
+  "password": "$2a$10$hWcb9Z29ud3YNPJgafLPeO/7fF6jE77mXri8Rv3TaoCtCsLPUNh6a", // Adm1nistrad0r
+  "role": "ADMIN_ROLE",
+  "status": "ACTIVE",
+  "createdAt": new Date(),
+  "updatedAt": new Date()
+});
--- init.js
+++ init.js
@@ -1,7 +1,5 @@
-db = db.getSiblingDB('etsisi');
-
-const collectionList = ["reservations", "rooms", "schedules", "users"];
+db = db.getSiblingDB('etsisi')
+const collectionList = ["reservations", "rooms", "schedules", "users"]
 collectionList.map(
   (collection) => {db.createCollection(collection)}
-);
-
+)
